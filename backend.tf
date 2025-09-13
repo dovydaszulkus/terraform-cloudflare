2f5fe4b63bd99737593db8cde8789099e339a64f
Dovydas Zulkus <15254019+dovydaszulkus@users.noreply.github.com>
2025-09-13 19:28:37 +0100

    refactor: rename Terraform remote state bucket

--- backend.tf
+++ backend.tf
@@ -1,13 +1,13 @@
 terraform {
   backend "s3" {
-      bucket = "terraform-cloudflare-test"
-      key    = "terraform.tfstate"
-      region                      = "auto"
-      skip_credentials_validation = true
-      skip_metadata_api_check     = true
-      skip_region_validation      = true
-      skip_requesting_account_id  = true
-      skip_s3_checksum            = true
-      use_path_style              = true
-    }
+    bucket                      = "terraform-cloudflare-infrastructure"
+    key                         = "terraform.tfstate"
+    region                      = "auto"
+    skip_credentials_validation = true
+    skip_metadata_api_check     = true
+    skip_region_validation      = true
+    skip_requesting_account_id  = true
+    skip_s3_checksum            = true
+    use_path_style              = true
+  }
 }
