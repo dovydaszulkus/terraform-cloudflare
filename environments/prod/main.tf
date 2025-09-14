dc9f209f2751483d8acf316b7b187166f9df1484
Dovydas Zulkus <15254019+dovydaszulkus@users.noreply.github.com>
2025-09-14 23:24:11 +0100

    chore: format Terraform files

--- environments/prod/main.tf
+++ environments/prod/main.tf
@@ -1,13 +1,13 @@
 module "web_app_bucket" {
   source = "../../modules/r2"
-  
-  account_id = var.account_id
+
+  account_id  = var.account_id
   bucket_name = "web-app-assets-prod"
 }
 
 module "web_app_worker" {
   source = "../../modules/workers"
-  
-  account_id = var.account_id
+
+  account_id  = var.account_id
   worker_name = "web-app-worker-prod"
 }
